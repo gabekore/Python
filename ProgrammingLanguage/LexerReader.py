85d8a146f5a879d1b7f5d75687dcc9d9ec35193d
Keiichi NAGAOKA <boottechprogramming@gmail.com>
2018-06-05 05:26:52 +0900

    コメント追加

--- ProgrammingLanguage/LexerReader.py
+++ ProgrammingLanguage/LexerReader.py
@@ -1,4 +1,15 @@
+#! /usr/bin/env python3
+
+
+#************************************************************
+# 字句の取得及びungetc的な機能
+#
+# 機能
+# ・読み込んだトークンを戻す（C言語のungetcみたいなもの）
+#************************************************************
+
 class LexerReader:
+
   reader = None
   unget_p = False
   ch = 0
@@ -7,11 +18,15 @@ class LexerReader:
   #---------------------------
   # コンストラクタ
   #
-  # tw : TextIOWrapper
+  # sio : io.StringIO
   #---------------------------
-  def __init__(self, r):
-    self.reader = r
+  def __init__(self, sio):
+    self.reader = sio
 
+  #---------------------------
+  # 次の文字を取得
+  # unget_pがTrueなら前回読んだ文字を返す
+  #---------------------------
   def read(self):
     if self.unget_p:
       self.unget_p = False
@@ -20,5 +35,9 @@ class LexerReader:
     
     return self.ch
   
+  #---------------------------
+  # unget_pフラグをTrueにし、
+  # 次のread()で現在の文字を返すようにする
+  #---------------------------
   def unread(self,c):
     self.unget_p = True
